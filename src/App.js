459c606e14d7fe82b58dabcaab880dc600adfc35
carbar-bsoft <carles.cbs@protonmail.ch>
2022-05-16 14:55:32 +0200

    starts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
+import Products from './components/Products';
+import Ratings from './components/Ratings';
 
 function App() {
+  const isValid = true;
+
   return (
     <div>
-     <h1>
-       Aprende React
-     </h1>
+      <Ratings rating='1' />
+      <Ratings rating='2' />
+      <Ratings rating='3' />
+      <Ratings rating='4' />
+      <Ratings rating='5' />
+      {/* <Products />
+      <Button variant="primary" disabled= {!isValid}>Default</Button> */}
     </div>
   );
 }
